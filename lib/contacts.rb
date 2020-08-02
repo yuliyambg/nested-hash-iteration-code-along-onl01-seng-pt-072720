26277a39c4e76e23f434adb55ae2b8eb6e39a99f
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-02 15:46:17 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,10 +16,12 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    if person == "Freddy Mercury"
+  if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-      binding.pry
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
     end
   end
-end
 end
